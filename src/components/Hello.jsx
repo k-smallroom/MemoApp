e307e76c7a5411a55a4619528608fefa6d8cc266
kohei <82309237+k-smallroom@users.noreply.github.com>
2021-12-02 01:23:03 +0900

    Create Hello component

---
+++ src/components/Hello.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import { bool, shape, string } from 'prop-types';
+
+function Hello(props) {
+    // props.children --- 親componentのタグの間に入れられた要素を表示する  ---
+    const { children, bang, style } = props;
+    return (
+        <View>
+            <Text style={[styles.text, style]}>
+                {`Hello ${children}${bang ? '!' : ''}`}
+            </Text>
+        </View>
+    );
+}
+
+Hello.propTypes = {
+    children: string.isRequired,
+    bang: bool,
+    style: shape(),
+};
+
+Hello.defaultProps = {
+    bang: false,
+    style: null,
+};
+
+const styles = StyleSheet.create({
+    text: {
+        color: '#ffffff',
+        backgroundColor: 'blue',
+        fontSize: 40,
+        fontWeight: 'bold',
+        padding: 16,
+    }
+});
+
+export default Hello;
