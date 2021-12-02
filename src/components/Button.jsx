83d17257a76198dae73b19bfacd4b1aa1e1e5a3b
kohei <82309237+k-smallroom@users.noreply.github.com>
2021-12-02 23:21:22 +0900

    Connect Screens with react-navigation

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -5,7 +5,7 @@ import { func, string } from "prop-types";
 export default function Button(props) {
     const { label, onPress } = props;
     return (
-        <TouchableOpacity style={styles.buttonContainer}>
+        <TouchableOpacity style={styles.buttonContainer} onPress={onPress}>
             <Text style={styles.buttonLabel}>{label}</Text>
         </TouchableOpacity>
     );
